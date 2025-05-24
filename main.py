a78026aac0a8f2e37baa7c801959bbee5c7dbb42
lorenrich <loren.m.rich@gmail.com>
2025-05-24 12:27:24 +0000

    Create screen and constants

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Colors
+    black = "#000000"
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        screen.fill(black)
+
+        # Refresh the screen
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
