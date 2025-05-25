08f1360903b8f4b9d9131c5b8d70dc6fdeb9b880
lorenrich <loren.m.rich@gmail.com>
2025-05-25 15:16:35 +0000

    Create ship, movement, and groups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -10,13 +11,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # Colors
-    black = "#000000"
+    black = (0, 0, 0)
 
     # Clock
     clock = pygame.time.Clock()
     clock.tick(60)
-    dt = 0/1000
 
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    radius = PLAYER_RADIUS
+    player = Player(x, y, radius)
+
+    dt = 0
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,9 +38,19 @@ def main():
     
         screen.fill(black)
 
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+        # Update updatable objects
+        updatable.update(dt)
+
+        # For object in the drawable group, draw it on the screen
+        for i in drawable:
+            i.draw(screen)
+        
+
         # Refresh the screen
         pygame.display.flip()
-
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -3,12 +3,12 @@ from circleshape import *
 from constants import *
 
 class Player(CircleShape):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
         self.x = x
         self.y = y
-        self.PLAYER_RADIUS = PLAYER_RADIUS
-        rotation = 0
+        self.radius = PLAYER_RADIUS
+        self.rotation = 0
 
         # in the player class
     def triangle(self):
@@ -20,9 +20,29 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        # TODO you need to review poly notes and overwrite this
-        # sub-classes must override
-        pass
-
+        white = (255, 255, 255)
+        line_width = 2
+        pygame.draw.polygon(screen, white, self.triangle(), line_width)
+        return
+    
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
 
     
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt)
+
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
